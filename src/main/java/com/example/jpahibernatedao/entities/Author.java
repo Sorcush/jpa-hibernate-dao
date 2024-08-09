b25cdc15239964fcd60b0ff45418f5d490c7f655
Andrey Sloutsman <sorcus@gmail.com>
2024-08-09 18:36:29 -0400

    Initial commit

---
+++ src/main/java/com/example/jpahibernatedao/entities/Author.java
@@ -0,0 +1,32 @@
+package com.example.jpahibernatedao.dao;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Transient  // we don't want this entity to have books list in db
+    private List<Book> books;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/com/example/jpahibernatedao/JpaHibernateDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpahibernatedao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
